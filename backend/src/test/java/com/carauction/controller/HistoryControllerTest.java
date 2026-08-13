7288360fb3420f4471ed3028614475f22f903ff7
ivanss-git <ivan.ibarracs@gmail.com>
2026-08-13 16:42:14 -0500

    docs: adding history controller test

--- backend/src/test/java/com/carauction/controller/HistoryControllerTest.java
+++ backend/src/test/java/com/carauction/controller/HistoryControllerTest.java
@@ -1,5 +1,34 @@
 package com.carauction.controller;
 
-public class HistoryControllerTest {
-    
+import com.carauction.service.AnalysisService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@WebMvcTest(HistoryController.class)
+class HistoryControllerTest {
+
+    // Prepare, Act, Perform.
+    // Tells spring to provide an object it created; Fake http client
+    @Autowired
+    private MockMvc mockMvc;
+
+    // Creating the fake Service
+    @MockitoBean
+    private AnalysisService service;
+
+    // Then to simulate the entire request and verify completion
+    @Test 
+    void deleteAnalysisReturnsNoContent() throws Exception {
+        mockMvc
+        .perform(delete("/api/vehicles/1/history"))
+        .andExpect(status().isNoContent());
+
+        verify(service).delete(1L);
+    }
 }
