7baf011a730b129704c233c09d68430ea26f38c4
ivanss-git <ivan.ibarracs@gmail.com>
2026-08-20 18:02:25 -0500

    feat: adding auction summary to site

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,9 +6,46 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+// typescript allows for the use of html inside of it
+// ts handles logic and imports
+// tsx/tsx is the html type code that allows for js
+// this all eventually turns into html
+
+// <button></button> to create a button
+// <code></code> to crete a border around the text
+// <img></img> to ensert an img, but most already be exported to import the src=
+// <section></section> for a new block of content
+// <h1></h1> creates a header of size 1
+// <h2></h2> create a header of size 2
+// <code></code> tells the browser or search engine that the text is actual prog lang code
+// <div></div> or <span></span> is a generic layout box used for styling anything
+  // allows you yo group elements together for styling 
+// <p></p> is the actual text that goes inside something like a div
+// <a></a> allows for a clickable hyperlink to another webpage, href att to know were to send the user
+// <ul></ul> creates a bulleted unordered list
+// <li></li> acts as a single list item, always inside <ul> or <ol>
+// <svg> and <use> work together to display sharp scalable icons that dont loose quality when yuo zoom
 
   return (
-    <>
+    <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: '100vh',
+        width: '100vw',
+        gap: '40px',
+        padding: '20px 0'
+      }}>
+      
+      {/* display: 'flex; forces everything to go from left to right */}
+      <section style={{ display: 'flex', gap: '24px' }}>
+        <div>Total Capital Invested</div>
+        <div>Total Gross Profit</div>
+        <div>Revenue</div>
+        <div>Average ROI:</div> 
+      </section>
+
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
@@ -16,7 +53,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1> Hello </h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
@@ -54,6 +91,7 @@ function App() {
             </li>
           </ul>
         </div>
+
         <div id="social">
           <svg className="icon" role="presentation" aria-hidden="true">
             <use href="/icons.svg#social-icon"></use>
@@ -115,7 +153,7 @@ function App() {
 
       <div className="ticks"></div>
       <section id="spacer"></section>
-    </>
+    </div>
   )
 }
 
